05b32b164e16cb607c3d15253bc47023b0406ee0
Darren Seet Serkai <darren@seet.me>
2014-05-15 09:21:44 +0900

    Complete module 6 Bootstrap Plugins

--- js/site.js
+++ js/site.js
@@ -9,4 +9,30 @@
         var reason = $(this).text();
         $pickButton.text(reason);
     });
+
+    var $sentDialog = $("#sentDialog");
+    $('#contactForm').on("submit", function () {
+        $sentDialog.modal('show');
+        return false;
+    });
+
+    var sentAlert = $('#sentAlert');
+
+    $sentDialog.on('hidden.bs.modal', function () {
+        sentAlert.show();
+    });
+
+    $('#contactForm input[type=submit]').tooltip({
+        delay: {
+            show: 500,
+            hide: 0
+        }
+    });
+
+    sentAlert.on('close.bs.alert', function () {
+        sentAlert.hide();
+        return false;
+    });
+
+    $('#theCarousel').carousel();
 })();
